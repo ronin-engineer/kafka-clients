9be153a912c0acbe40f611c5c663226faf0d67f4
vking34 <vuong.lv34@gmail.com>
2024-04-01 14:43:27 +0700

    chore(producer): rm redundant

--- src/main/java/dev/ronin_engineer/kafka/clients/Producer.java
+++ src/main/java/dev/ronin_engineer/kafka/clients/Producer.java
@@ -3,7 +3,6 @@ package dev.ronin_engineer.kafka.clients;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 
